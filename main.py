319804b61e5380136a29e1f5c0af554d07270066
Amr <kamr08985@gmail.com>
2025-03-04 22:46:16 +0200

    Added till e

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import numpy as np
 import pandas as pd
 from pandas import read_csv
-dataset=read_csv('Obesity.csv',header=None)
+dataset=pd.read_csv('Obesity.csv')
+dataset = dataset.convert_dtypes()
 #print(dataset)
-
 #first part of the requirement tasks (Data Analysis task)
 #a)Display the first 12 rows of the dataset(Obesity)
 print(dataset.head(12))
+print("========================================================================================")
 #a)Display the last 12 rows of the dataset(Obesity)
 print(dataset.tail(12))
+print("========================================================================================")
+#b)Identify and print the total number of rows and columns present.
+print(dataset.shape)
+print("========================================================================================")
+#c)List all column names along with their corresponding data types
+print(dataset.dtypes)
+print("========================================================================================")
+#d)Print the name of the first column.
+print(dataset.columns[0])
+print("========================================================================================")
+#e) Generate a summary of the dataset, including non-null counts and data types.
+print(dataset.info())
