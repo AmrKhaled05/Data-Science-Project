41e5af1af3eb5536cfc3e620261001c9613bd932
Amr <kamr08985@gmail.com>
2025-03-04 22:10:43 +0200

    Added a

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ import numpy as np
 import pandas as pd
 from pandas import read_csv
 dataset=read_csv('Obesity.csv',header=None)
-print(dataset)
+#print(dataset)
+
+#first part of the requirement tasks (Data Analysis task)
+#a)Display the first 12 rows of the dataset(Obesity)
+print(dataset.head(12))
+#a)Display the last 12 rows of the dataset(Obesity)
+print(dataset.tail(12))
