8f12482ae4dc07d6f732358a42c9e8e87d858e56
Amr <kamr08985@gmail.com>
2025-03-04 22:51:21 +0200

    fixed the datatype conversions from the dataset when printed out

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
 from pandas import read_csv
-dataset=read_csv('Obesity.csv',header=None)
-print(dataset)
+dataset=read_csv('Obesity.csv')
+dataset = dataset.convert_dtypes()
+#run it first before writing any code to ensure the dataset is printed out
+#print(dataset)
